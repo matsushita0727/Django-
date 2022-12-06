af65ede48968caa94013b35149b016f312abd939
Muratake <206027@std.hi-joho.ac.jp>
2022-12-06 16:05:21 +0900

    stock 更新

--- Project_Folder/settings.py
+++ Project_Folder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,12 +41,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'django_filters',
+    'pure_pagination',
+    'bootstrap4',
+    'invoice',
     "Customer.apps.CustomerConfig",
     "App_Folder.apps.AppFolderConfig",
     "stock.apps.StockConfig",
